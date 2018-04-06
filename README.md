9be20fd86e08c886d2978855309aa2105660bf8d
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-04-06 10:06:39 +0100

    Done all the code as per the lecture. Is actually not working getting misconfigured csrf error. Looking at is actuall code there are diferences

--- README.md
+++ README.md
@@ -2,3 +2,46 @@
 Authentication example from scratch
 
 One of the ways to do authentication that goes a bit deeper than normal.
+
+
+Security Best Practices
+
+- Always use SSL
+- Use Cookie Flags
+
+    app.use(session({
+        cookieName: 'session',
+        secret: 'some_random_string',
+        duration: 30 * 60 * 1000,
+        activeDuration: 5 * 60 * 1000,
+        httpOnly: true, // don't let JS code access cookies
+        secure: true,   // only set cookies over https
+        ephemeral: true // destroy cookies when the browser closes
+    }));
+
+- Wear a Helmet
+    npm install helmet
+
+    const helmet = require('helmet');
+
+    //manage http header security
+    app.use(helmet());
+
+- Don't roll your own
+    i.e.- don't use this code, instead use 
+        passport
+        node-login
+        aqua
+        Okta
+
+        okta is developed by the Guy that provided the lecture and code.
+        worth looking into this package further as passport still has 
+        issues(you are still doing a lot yourself).
+        
+        https://www.npmjs.com/package/@okta/okta-auth-js
+
+
+Ckeck out the code @ https://github.com/rdegges/ss-auth
+Check the slides @ http://speaerdeck.com/rdegges
+
+
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-
-const bodyParser = require('body-parser');
+const express =     require('express');
+const bodyParser =  require('body-parser');
+const sessions =    require('client-sessions');
+const bcrypt =      require('bcryptjs');
+const csurf =       require('csurf');
+const helmet =      require('helmet');
 
 //require DB connection module
 const mongoose = require('mongoose');
@@ -14,21 +17,59 @@ let User = mongoose.model('User', new mongoose.Schema({
     password:   { type: String, required: true },
 }));
 
+/************* loginRequired middleware ********************* */
+function loginRequired(req, res, next){
+    if(!req.user){
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
 let app = express();
 
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({
     extended: false
-}))
+}));
+
+/*******CSRF Cross Site Request Forgery handling ********** */
+app.use(csurf());
+/* now add { csrfToken: req.csrfToken() } to any places that call render
+and have a form */
+
+app.use(sessions({
+    cookieName: 'session',
+    secret: 'qapernfhd3241edrf',
+    duration: 30 * 60 * 1000,    //30 mins
+    activeDuration: 5 * 60 * 1000, //5 minutes
+    httpOnly: true, // don't let JS code access cookies
+    secure: true,   // only set cookies over https
+    ephemeral: true // destroy cookies when the browser closes
+}));
+
+
+/**********manage http header security****** */
+app.use(helmet());
+
+/**************************************************************************** */
+/**                        Routes                                             */
+/**************************************************************************** */
 app.get('/', (req, res)=>{
     res.render('index');
 });
 
+/************************* register Routes *********************************** */
 app.get('/register', (req, res)=>{
-    res.render('register');
+    res.render('register', { csrfToken: req.csrfToken() });
 });
 
 app.post('/register', (req, res)=>{
+    /* 14 is the encryption factor for bcrypt. The higher the level 
+    the safer it will be, but it will also take longer ot calculate, so use
+    it care. 14 is the current feasible value being used */
+    let hash = bcrypt.hashSync(req.body.password, 14);
+    req.body.password = hash;
     let user = new User(req.body);
     user.save((err)=>{
         if(err){
@@ -38,33 +79,49 @@ app.post('/register', (req, res)=>{
                 error = 'That email is already taken, please try another.'
             }
 
-            return res.render('register', {error:error});
+            return res.render('register', {
+                error:error,
+                csrfToken: req.csrfToken()
+            });
         }
 
         res.redirect('/dashboard');
     });
 
 });
-
+/****************************** login Routes ************************************* */
 app.get('/login', (req, res)=>{
-    res.render('login');
+    res.render('login', { csrfToken: req.csrfToken() });
 });
 
 app.post('/login', (req, res)=>{
     User.findOne({ email: req.body.email }, (err, user)=>{
-        if(err || !user || req.body.password != user.password){
+        if(!user || !bcrypt.compareSync(req.body.password, user.password)){
             return res.render('login', {
                 error: 'Incorrect email / password.'
             });
         }
+        req.session.userId = user._id;
         res.redirect('/dashboard');
     });
 });
-
-app.get('/dashboard', (req, res)=>{
-    res.render('dashboard');
+/********************** dashboard routes ******************************************** */
+app.get('/dashboard', loginRequired, (req, res, next)=>{
+    if(!(req.session && req.session.userId)){
+        return res.redirect('/login');
+    }
+    User.findById(req.session.userId, (err, user)=>{
+        if(err){
+            return next(err);
+        }
+        if(!user){
+            return res.redirect('/login');
+        }
+        res.render('dashboard');
+    });
 });
 
+/****************************************************************** */
 app.listen(3000, ()=>{
     console.log('server running on port 3000');
 });
